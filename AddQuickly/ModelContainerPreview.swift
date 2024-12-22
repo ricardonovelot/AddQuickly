2f6f542def2f190ea3b8c8291365f14c9b4cdb77
ricardonovelot <ricardonovelo97.tools@gmail.com>
2024-12-21 23:00:57 -0600

    first commit, add quick entries and fixes

---
+++ AddQuickly/ModelContainerPreview.swift
@@ -0,0 +1,8 @@
+//
+//  ModelContainerPreview.swift
+//  AddQuickly
+//
+//  Created by Ricardo on 21/12/24.
+//
+
+import Foundation
---
+++ SampleData.swift
@@ -0,0 +1,8 @@
+//
+//  SampleData.swift
+//  AddQuickly
+//
+//  Created by Ricardo on 21/12/24.
+//
+
+import Foundation
